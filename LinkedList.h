9514341cea4ff1ca7fd56efa49c45473f8b9a7bc
Sam Greene <sammygreene@gmail.com>
2021-10-22 15:49:25 -0600

    Start

---
+++ LinkedList.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <"LinkedListInterface.h">
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+template<typename T>
+class LinkedList : public LinkedListInterface<T>
+{
+    
+};
